612c1db26089bbec7c41d5dbe3be5d022c846c0c
Doruk Bekatli (dbekatli) <doruk.bekatli@gmail.com>
2025-11-29 23:41:32 +0100

    change cva6 config to cv64a6_imafdchsclic_sv39_wb

--- bender.mk
+++ bender.mk
@@ -6,6 +6,6 @@
 # Lorenzo Leone <lleone@iis.ee.ethz.ch>
 
 COMMON_TARGS ?=
-COMMON_TARGS += -t snitch_cluster -t cv64a6_imafdc_sv39 -t cva6 -t rtl
-
+COMMON_TARGS += -t snitch_cluster -t cv64a6_imafdchsclic_sv39_wb -t cva6 -t rtl
+# cv64a6_imafdchsclic_sv39_wb
 SIM_TARGS = $(COMMON_TARGS) -t test -t sim
